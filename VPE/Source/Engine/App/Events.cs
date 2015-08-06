bc9143f08e1e75669b5623a77dc4c89a30e5117d
kuviman <kuviman@gmail.com>
2015-08-06 22:35:44 +0300

    App events

--- VPE/Source/Engine/App/Events.cs
+++ VPE/Source/Engine/App/Events.cs
@@ -40,10 +40,21 @@ namespace VitPro.Engine {
 
 		static Timer timer = new Timer();
 
+		public static event Action OnRender;
+		public static event Action<double> OnUpdate;
+		public static event Action<Key> OnKeyDown;
+		public static event Action<Key> OnKeyUp;
+		public static event Action<char> OnKeyPress;
+		public static event Action<MouseButton, Vec2> OnMouseDown;
+		public static event Action<MouseButton, Vec2> OnMouseUp;
+		public static event Action<Vec2> OnMouseMove;
+		public static event Action<double> OnMouseWheel;
+
 		static void InitEvents() {
 			log.Info("Registering window events");
 			window.RenderFrame += (sender, e) => {
 				timer.Tick();
+				OnRender.Apply();
 				if (State != null)
 					State.Render();
 				if (RenderState.version != 0)
@@ -52,6 +63,7 @@ namespace VitPro.Engine {
 				window.SwapBuffers();
 			};
 			window.UpdateFrame += (sender, e) => {
+				OnUpdate.Apply(e.Time);
 				var state = State;
 				if (state != null) {
 					state.Update(e.Time);
@@ -62,6 +74,7 @@ namespace VitPro.Engine {
 					Kill();
 			};
 			window.KeyDown += (sender, e) => {
+				OnKeyDown.Apply((Key)e.Key);
 				if (State != null) {
 					if (e.IsRepeat)
 						State.KeyRepeat((Key)e.Key);
@@ -72,26 +85,32 @@ namespace VitPro.Engine {
                     window.Close();
 			};
 			window.KeyUp += (sender, e) => {
+				OnKeyUp.Apply((Key)e.Key);
 				if (State != null)
 					State.KeyUp((Key)e.Key);
 			};
 			window.KeyPress += (sender, e) => {
+				OnKeyPress.Apply(e.KeyChar);
 				if (State != null)
 					State.CharInput(e.KeyChar);
 			};
 			window.MouseDown += (sender, e) => {
+				OnMouseDown.Apply((MouseButton)e.Button, FixMouse(Mouse.Position));
 				if (State != null)
 					State.MouseDown((MouseButton)e.Button, FixMouse(Mouse.Position));
 			};
 			window.MouseUp += (sender, e) => {
+				OnMouseUp.Apply((MouseButton)e.Button, FixMouse(Mouse.Position));
 				if (State != null)
 					State.MouseUp((MouseButton)e.Button, FixMouse(Mouse.Position));
 			};
 			window.MouseMove += (sender, e) => {
+				OnMouseMove.Apply(FixMouse(Mouse.Position));
 				if (State != null)
 					State.MouseMove(FixMouse(Mouse.Position));
 			};
 			window.MouseWheel += (sender, e) => {
+				OnMouseWheel.Apply(e.DeltaPrecise);
 				if (State != null)
 					State.MouseWheel(e.DeltaPrecise);
 			};
@@ -101,10 +120,7 @@ namespace VitPro.Engine {
 		}
 
 		static Vec2 FixMouse(Vec2 pos) {
-//			if (Multisampling)
-//				return pos * multisampleFactor;
-//			else
-				return pos;
+			return pos;
 		}
 
 	}
